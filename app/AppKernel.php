787dcf3a815e1727cb5340ee3bc21588f20e60ab
Leny Bernard <leny@troopers.email>
2017-11-24 11:17:31 +0100

    install markdown-bundle and twig-extensions (for truncate text essentially)

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Contentful\ContentfulBundle\ContentfulBundle(),
+            new Knp\Bundle\MarkdownBundle\KnpMarkdownBundle(),
             new AppBundle\AppBundle(),
         ];
 

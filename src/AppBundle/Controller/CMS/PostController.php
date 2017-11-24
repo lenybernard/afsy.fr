66746e01d384a43fd155cc75a10b33e93de0f06c
Leny Bernard <leny@troopers.email>
2017-11-24 19:35:34 +0100

    add listByCategory action and refactor with custom advent's one (+make links everywhere)

--- src/AppBundle/Controller/CMS/PostController.php
+++ src/AppBundle/Controller/CMS/PostController.php
@@ -2,8 +2,10 @@
 
 namespace AppBundle\Controller\CMS;
 
+use Symfony\Bridge\Twig\TwigEngine;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Templating\EngineInterface;
 
 class PostController extends Controller
 {
@@ -26,22 +28,6 @@ class PostController extends Controller
     }
 
     /**
-     * @Route("/calendrier-avent-2017")
-     */
-    public function advent2017Action()
-    {
-        $client = $this->get('contentful.delivery');
-        $query = new \Contentful\Delivery\Query;
-        $query->setContentType(self::CONTENT_TYPE_POST)
-            ->where('fields.category.sys.id', '2xh0nTld5O8kIsu0Qqs0IQ')
-            ->orderBy('fields.date');
-
-        return $this->render('cms/post/advent.html.twig', [
-            'entries' => $client->getEntries($query)
-        ]);
-    }
-
-    /**
      * @Route("/blog/{slug}")
      */
     public function showAction($slug)
@@ -54,6 +40,37 @@ class PostController extends Controller
 
         return $this->render('cms/post/show.html.twig', [
             'post' => $client->getEntries($query)[0]
+        ]);
+    }
+
+    /**
+     * @Route("/blog/category/{slug}")
+     */
+    public function listByCategoryAction($slug, EngineInterface $twigEngine)
+    {
+        $client = $this->get('contentful.delivery');
+        //find first the category
+        $query = new \Contentful\Delivery\Query;
+        $query->setContentType(self::CONTENT_TYPE_CATEGORY)
+            ->where('fields.slug', $slug)
+            ->setLimit(1);
+        $category = $client->getEntries($query)[0];
+
+        //find posts by category
+        $query = new \Contentful\Delivery\Query;
+        $query->setContentType(self::CONTENT_TYPE_POST)
+            ->where('fields.category.sys.id', $category->getId())
+            ->orderBy('fields.date');
+
+        //seek for category custom template
+        $template = sprintf('cms/category/custom/%s.html.twig', $slug);
+        if (!$twigEngine->exists($template) ) {
+            $template = 'cms/post/index.html.twig';
+        }
+
+        return $this->render($template, [
+            'title' => $category->getTitle(),
+            'entries' => $client->getEntries($query)
         ]);
     }
 
